40475e847bf99625df26f30b1f8b98e4e44bc879
cuongnpvn <18520545@gm.uit.edu.vn>
2021-05-28 18:09:47 +0700

    'cuongng'

---
+++ wallpaperChange/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace wallpaperChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "newBackground.jpg";
+
+            // download new background
+            new WebClient().DownloadFile("https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697", fileName);
+            string imagePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            changeBackground(imagePath+fileName);
+        }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01; 
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        static void changeBackground(string filename)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 1, filename, SPIF_UPDATEINIFILE);
+        }
+    }
+}
